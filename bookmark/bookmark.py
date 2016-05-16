2a6997f2c7cf0e1e33e2b63e7ad64c6b60603977
idealegg <452144941@qq.com>
2016-05-16 15:30:45 +0800

    first usable version

--- bookmark/bookmark.py
+++ bookmark/bookmark.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import re
+import pprint
+
 
 type_list = ('                    <DT><A HREF',
 '            </DL><p>',
@@ -8,43 +11,125 @@ type_list = ('                    <DT><A HREF',
 '        </DL><p>',
 '        <DL><p>',
 '        <DT><H3 ADD_DATE',
+'    </DL><p>',
+'    <DL><p>',
+'    <DT><H3 ADD_DATE',
 None
 )
 
 class BookMark:
-    '''
-                    <DT><A HREF
-            </DL><p>
-            <DL><p>
-            <DT><A HREF
-            <DT><H3 ADD_DATE
-        </DL><p>
-        <DL><p>
-        <DT><H3 ADD_DATE
-     DO NOT EDIT! -->
-     It will be read and overwritten.
-    </DL><p>
-    <DL><p>
-    <DT><H3 ADD_DATE
-<!-- This is an automatically generated file.
-<!DOCTYPE NETSCAPE-Bookmark-file-1>
-</DL><p>
-<DL><p>
-<H1>Bookmarks</H1>
-<META HTTP-EQUIV
-<TITLE>Bookmarks</TITLE>
-    '''
+    __doc__ = ('\n'
+               '                    <DT><A HREF\n'
+               '            </DL><p>\n'
+               '            <DL><p>\n'
+               '            <DT><A HREF\n'
+               '            <DT><H3 ADD_DATE\n'
+               '        </DL><p>\n'
+               '        <DL><p>\n'
+               '        <DT><H3 ADD_DATE\n'
+               '     DO NOT EDIT! -->\n'
+               '     It will be read and overwritten.\n'
+               '    </DL><p>\n'
+               '    <DL><p>\n'
+               '    <DT><H3 ADD_DATE\n'
+               '<!-- This is an automatically generated file.\n'
+               '<!DOCTYPE NETSCAPE-Bookmark-file-1>\n'
+               '</DL><p>\n'
+               '<DL><p>\n'
+               '<H1>Bookmarks</H1>\n'
+               '<META HTTP-EQUIV\n'
+               '<TITLE>Bookmarks</TITLE>\n'
+               '    ')
 
     def __init__(self, line_no, content):
         self.type = None
         self.line_no = line_no
         self.content = content
+        self.key = None
         self.child_list = []
+        self.parent = None
+        self.to_inserted_behind = []
+        self.checked = False
 
-    def init_type(self):
+    def initialize(self):
         global type_list
         for t_type in type_list:
-            if self.content[:len(t_type)] == t_type:
-                self.type = t_type
+            if t_type:
+                if self.content[:len(t_type)] == t_type:
+                    self.type = t_type
+                    break
+        re_result = re.search(r'>([\w+]+)</H3>', self.content)
+        if re_result:
+            self.key = re_result.group(1)
+        else:
+            re_result = re.search(r'<DT><A HREF="([^"]+)"', self.content)
+            if re_result:
+                self.key = re_result.group(1)
 
+    def is_child(self, t_type):
+        if not self.type:
+            return False
+        if self.type[:4] != '    ':
+            return False
+        if self.type[-16:] != '<DT><H3 ADD_DATE':
+            return False
+        ret_list = []
+        ret_list.append(''.join(['    ', self.type]))
+        ret_list.append(''.join(['    ', self.type[:-16], '<DT><A HREF']))
+        if t_type in ret_list:
+            return True
+        return False
 
+    def is_grandchild(self, t_type):
+        if not self.type or not t_type or self.type[:4] != '    ' or self.type[-16:] != '<DT><H3 ADD_DATE':
+            return False
+        if t_type.find(''.join(['        ', self.type])) != -1 or t_type.find(''.join(['        ', self.type[:-16], '<DT><A HREF'])) != -1:
+            return True
+        return False
+
+    def is_p(self):
+        if self.type and self.type.find('<p>') != -1:
+            return True
+        return False
+
+    def is_ahref(self):
+        if self.type and self.type.find('<DT><A HREF=') != -1:
+            return True
+        return False
+
+    def is_directory(self):
+        if self.type and self.type.find('<DT><H3 ADD_DATE=') != -1:
+            return True
+        return False
+
+    def is_in(self, a_href):
+        if self.key and self.key == a_href:
+            return True
+        return False
+
+    def get_parent_and_inserted_lines(self):
+        if self.is_ahref():
+            pass
+
+    def checked(self):
+        self.checked = True
+        for c_item in self.child_list:
+            c_item.checked()
+
+    def get_domain(self):
+        domain = self.line_no
+        if self.child_list:
+            domain = self.child_list[-1].get_domain() + 1
+        return domain
+
+    def print_bookmark(self):
+        if self.key:
+            print self.key+"\n"
+        else:
+            print "CONTENT: {0} LENGTH {1}".format(self.content, len(self.content))
+        if self.type:
+            print "type: {0}".format(self.type)
+        else:
+            print "type: None"
+        pprint.pprint(self.child_list)
+        pprint.pprint(self.parent)
--- bookmarkmanager/bookmarkmanager.py
+++ bookmarkmanager/bookmarkmanager.py
@@ -1,23 +1,84 @@
 # -*- coding: utf-8 -*-
 
 import bookmark.bookmark
+import pprint
+
 
 class BookMarkManager:
     def __init__(self, file):
         self.file = file
         self.bookmark_list = []
+        self.lines = None
 
     def initialize(self):
-        lines = open(file, 'r').readlines()
+        t_file_o = open(self.file, 'r')
+        self.lines = t_file_o.readlines()
+        t_file_o.close()
         num = 0
-        type_stack = []
-        for line in lines:
+        #type_stack = []
+        for line in self.lines:
             num += 1
             line = line[:-1]
             bm = bookmark.bookmark.BookMark(num, line)
-            bm.init_type()
-            cur_type = bm.type
-            if len(type_stack):
-                last_type = type_stack[-1]
+            bm.initialize()
+            #cur_type = bm.type
+            #if len(type_stack):
+            #    last_type = type_stack[-1]
+            self.bookmark_list.append(bm)
+        num = 0
+        for bm_item in self.bookmark_list:
+            c_num = 0
+            for child_item in self.bookmark_list[num+1:]:
+                if not child_item.is_p():
+                    if bm_item.is_child(child_item.type):
+                        self.bookmark_list[num].child_list.append(child_item)
+                        self.bookmark_list[c_num].parent = self.bookmark_list[num]
+                    else:
+                        if bm_item.is_grandchild(child_item.type):
+                            continue
+                        else:
+                            break
+                c_num += 1
+            num += 1
+
+    def find_item(self, item):
+        for bm_item in self.bookmark_list:
+            if item.type == bm_item.type and item.key == bm_item.key:
+                return bm_item
+        return None
+
+    def get_inserted_item(self, i_bmm):
+        for i_item in i_bmm.bookmark_list:
+            # if is a a href link, to judge if insert.
+            if i_item.is_ahref():
+                o_item = self.find_item(i_item)
+                # if not found, insert
+                to_insert = None
+                while not o_item:
+                    to_insert = i_item
+                    i_item = i_item.parent
+                    o_item = self.find_item(i_item)
+                if not to_insert.checked:
+                    o_item.child_list[-1].to_inserted_behind.append(to_insert)
+                    i_item.checked()
+
+    def get_merge_lines(self, i_bmm):
+        t_lines = []
+        for m_item in self.bookmark_list:
+            t_lines.append(m_item.content)
+            for n_item in m_item.to_inserted_behind:
+                t_lines.extend(i_bmm.bookmark_list[n_item.line_no:n_item.get_domain()+1])
+        return t_lines
+
+if __name__ == '__main__':
+    bmm1 = BookMarkManager('../bookmarks.html')
+    bmm1.initialize()
+    bmm2 = BookMarkManager('../bookmarks_16_5_16.html')
+    bmm2.initialize()
+    bmm1.get_inserted_item(bmm2)
+    final_lines = bmm1.get_merge_lines(bmm2)
+    file_o = open('../bookmarks_output.html', 'w')
+    file_o.writelines(map(lambda x: ''.join([x, "\n"]), final_lines))
+    file_o.close()
 
 
